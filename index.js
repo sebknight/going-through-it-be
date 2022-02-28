423f04f7f0df674797a636eb60428e261c596a0b
sebknight <sebastianknightmaddox@gmail.com>
2022-02-28 19:57:12 +1300

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const cors = require('cors');
 const cache = require('memory-cache');
 const { body } = require('express-validator');
